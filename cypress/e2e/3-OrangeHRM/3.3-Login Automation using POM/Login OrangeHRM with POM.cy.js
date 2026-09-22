afe1b11c9160817441c21ae6d3095b5eea1f1744
phr4s <muhammadnurprasetyo@gmail.com>
2026-09-22 21:05:59 +0700

    Update TC logic and fix POM structure

--- cypress/e2e/3-OrangeHRM/3.3-Login Automation using POM/Login OrangeHRM with POM.cy.js
+++ cypress/e2e/3-OrangeHRM/3.3-Login Automation using POM/Login OrangeHRM with POM.cy.js
@@ -23,14 +23,12 @@ describe('Login OrangeHRM with POM', () => {
 
     // Verify Forgot your password? button hyperlink
     it('LP-03 Verify Forgot your password? button hyperlink', () => {
-        loginHRM.getForgotPasswordButton().click();
+        loginHRM.getForgotPasswordButton();
         loginHRM.getForgotPasswordLink();
     })
 
     // Verify mandatory field validation in the login page
     it('LP-04 Verify mandatory field validation in the login page', () => {
-        loginHRM.inputUsername().clear();
-        loginHRM.inputPassword().clear();
         loginHRM.clickLoginButton();
         loginHRM.getUsernameErrorMessage();
         loginHRM.getPasswordErrorMessage();
@@ -38,32 +36,30 @@ describe('Login OrangeHRM with POM', () => {
 
     //Verify login with registered password and empty username
     it('LP-05 Verify login with registered password and empty username', () => {
-        loginHRM.inputUsername().clear();
-        loginHRM.inputPassword().type(loginData.validPassword);
+        loginHRM.getPassword(loginData.validPassword);
         loginHRM.clickLoginButton();
         loginHRM.getUsernameErrorMessage();
     })
 
     // Verify login with registered username and empty password
     it('LP-06 Verify login with registered username and empty password', () => {
-        loginHRM.inputUsername().type(loginData.validUsername);
-        loginHRM.inputPassword().clear();
+        loginHRM.getUsername(loginData.validUsername);
         loginHRM.clickLoginButton();
         loginHRM.getPasswordErrorMessage();
     })
 
     // Verify login using non-registered account
     it('LP-07 Verify login using non-registered account', () => {
-        loginHRM.inputUsername().type(loginData.invalidUsername);
-        loginHRM.inputPassword().type(loginData.invalidPassword);
+        loginHRM.getUsername(loginData.invalidUsername);
+        loginHRM.getPassword(loginData.invalidPassword);
         loginHRM.clickLoginButton();
         loginHRM.getInvalidCredentialsErrorMessage();
     })
 
     // Verify login using registered account
     it('LP-08 Verify login using registered account', () => {
-        loginHRM.inputUsername().type(loginData.validUsername);
-        loginHRM.inputPassword().type(loginData.validPassword);
+        loginHRM.getUsername(loginData.validUsername);
+        loginHRM.getPassword(loginData.validPassword);
         loginHRM.clickLoginButton();
         loginHRM.getDashboardUrl();
     })
--- cypress/support/pageObjects/loginHRM.js
+++ cypress/support/pageObjects/loginHRM.js
@@ -1,37 +1,37 @@
 class LoginHRM {
     // Elements
     gotoPage() {
-        return cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
     }
     getLink() {
-        return cy.url().should('include', '/auth/login');
+        cy.url().should('include', '/auth/login');
     }
     getFavicon() {
-        return cy.get('link[rel="icon"]').should('have.attr', 'href').and('include', 'favicon.ico');
+        cy.get('link[rel="icon"]').should('have.attr', 'href').and('include', 'favicon.ico');
     }
     getBranding() {
-        return cy.get('.orangehrm-login-branding').should('be.visible');
+        cy.get('.orangehrm-login-branding').should('be.visible');
     }
     getLogo() {
-        return cy.get('.orangehrm-login-logo').should('be.visible');
+        cy.get('.orangehrm-login-logo').should('be.visible');
     }
     getForgotPasswordButton() {
-        return cy.get('.orangehrm-login-forgot-header',{timeout: 30000}).should('be.visible');
+        cy.get('.orangehrm-login-forgot-header',{timeout: 10000}).should('be.visible').click();
     }
     getForgotPasswordLink() {
-        return cy.url({timeout: 30000}).should('contain', '/requestPassword');
+        cy.url({timeout: 10000}).should('contain', '/requestPassword');
     }
-    inputUsername() {
-        return cy.get('input[name="username"]',{timeout: 30000});
+    getUsername(username) {
+        cy.get('input[name="username"]',{timeout: 10000}).type(username);
     }
-    inputPassword() {
-        return cy.get('input[name="password"]',{timeout: 30000});
+    getPassword(password) {
+        cy.get('input[name="password"]',{timeout: 10000}).type(password);
     }
     clickLoginButton() {
-        return cy.get('button[type="submit"]',{timeout: 15000}).click();
+        cy.get('button[type="submit"]',{timeout: 10000}).click();
     }
     getUsernameErrorMessage() {
-        return cy.get('.oxd-input-group')
+        cy.get('.oxd-input-group')
             .contains('Username')
             .parents('.oxd-input-group')
             .find('.oxd-input-field-error-message')
@@ -39,7 +39,7 @@ class LoginHRM {
             .and('have.text', 'Required');
     }
     getPasswordErrorMessage() {
-        return cy.get('.oxd-input-group')
+        cy.get('.oxd-input-group')
             .contains('Password')
             .parents('.oxd-input-group')
             .find('.oxd-input-field-error-message')
@@ -47,10 +47,10 @@ class LoginHRM {
             .and('have.text', 'Required');
     }
     getInvalidCredentialsErrorMessage() {
-        return cy.get('.oxd-alert-content',{timeout: 30000}).should('be.visible').and('have.text', 'Invalid credentials');
+        cy.get('.oxd-alert-content',{timeout: 10000}).should('be.visible').and('have.text', 'Invalid credentials');
     }
     getDashboardUrl() {
-        return cy.url({timeout: 30000}).should('include', '/dashboard');
+        cy.url({timeout: 10000}).should('include', '/dashboard');
     }
 }
 export default new LoginHRM();
